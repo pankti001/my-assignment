e0fdef4ead5880b0be7c9430b63d9781e5f17af2
Pankti0911 <pankti112004@gmail.com>
2026-06-24 18:55:16 +0530

    security protection added

---
+++ apps/web/src/routes/animation/+page.server.ts
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ locals }) {
+	const session = await locals.auth();
+
+	if (!session) {
+		throw redirect(302, '/login');
+	}
+
+	return { session };
+}
